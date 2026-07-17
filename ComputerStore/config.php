640dac5117316fabf56b60d5693498d8ecb51896
Renz-Programmer <figuerrarenz12@gmail.com>
2026-07-17 22:03:48 +0800

    Delete ComputerStore directory

--- ComputerStore/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = new mysqli("sql105.infinityfree.com", "if0_42394236", "sharmaineorange", "if0_42394236_computer_store");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-session_start();
-?>
